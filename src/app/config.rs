220827387351693e3e79766205049535cd5e8b99
André Meira <6381457+afsmeira@users.noreply.github.com>
2025-12-05 17:01:26 +0000

    test: Trigger Codacy analysis

--- src/app/config.rs
+++ src/app/config.rs
@@ -20,6 +20,6 @@ impl Config {
         }
     }
     pub fn load() {
-        dotenv().unwrap_or_else(|error| panic!("Cannot load config, sorry (not sorry. Actually sorry): {}", error));
+        dotenv().unwrap_or_else(|error| panic!("Cannot load config, very sorry: {}", error));
     }
 }
