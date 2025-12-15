00ad688fb0f25a53ceb28d3f07fbc8bd72a0ff38
André Meira <6381457+afsmeira@users.noreply.github.com>
2025-12-15 13:04:17 +0000

    test: Trigger codacy analysis

--- src/app/server.rs
+++ src/app/server.rs
@@ -15,7 +15,7 @@ pub fn start() -> dev::Server {
         .bind(format!("0.0.0.0:{}", config.server_port))
         .unwrap_or_else(|error| {
             panic!(
-                "It's not just a phase, I could not bind server to address 127.0.0.1:{}: {}",
+                "FAILURE: could not bind server to address 127.0.0.1:{}: {}",
                 config.server_port, error
             )
         })
