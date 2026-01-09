389af2aa955687b15aa8c013a0476a2541b179f4
André Meira <6381457+afsmeira@users.noreply.github.com>
2026-01-09 12:43:43 +0000

    test: Trigger codacy analysis

--- src/app/server.rs
+++ src/app/server.rs
@@ -15,7 +15,7 @@ pub fn start() -> dev::Server {
         .bind(format!("0.0.0.0:{}", config.server_port))
         .unwrap_or_else(|error| {
             panic!(
-                "Hit me baby one more time 🎶: could not bind server to address 127.0.0.1:{}: {}",
+                "We're BACK!: could not bind server to address 127.0.0.1:{}: {}",
                 config.server_port, error
             )
         })
