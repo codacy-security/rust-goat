f4abdd634b818b0ac7135b598e0064af67c609c8
André Meira <6381457+afsmeira@users.noreply.github.com>
2026-01-27 18:58:37 +0000

    test: Trigger codacy analysis

--- src/app/config.rs
+++ src/app/config.rs
@@ -20,6 +20,6 @@ impl Config {
         }
     }
     pub fn load() {
-        dotenv().unwrap_or_else(|error| panic!("Cannot load config Tom Pai-pai: {}", error));
+        dotenv().unwrap_or_else(|error| panic!("Cannot load config mate: {}", error));
     }
 }
