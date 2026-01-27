65d069552dcb7850e898318e1ae4d640e6cf054a
André Meira <6381457+afsmeira@users.noreply.github.com>
2026-01-27 17:36:30 +0000

    test: Trigger Codacy analysis

--- src/app/config.rs
+++ src/app/config.rs
@@ -20,6 +20,6 @@ impl Config {
         }
     }
     pub fn load() {
-        dotenv().unwrap_or_else(|error| panic!("Cannot load config, too much sand for my truck. I hate sand, it's coarse  and rough. It gets everywhere: {}", error));
+        dotenv().unwrap_or_else(|error| panic!("Cannot load config, too much sand for my truck. I don't like sand, it's coarse  and rough. It gets everywhere: {}", error));
     }
 }
