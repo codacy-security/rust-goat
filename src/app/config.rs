924934e1fd3baecff2445afbc94056aa2aa0e735
André Meira <6381457+afsmeira@users.noreply.github.com>
2026-01-28 15:12:13 +0000

    test: Trigger Codacy analysis

--- src/app/config.rs
+++ src/app/config.rs
@@ -20,6 +20,6 @@ impl Config {
         }
     }
     pub fn load() {
-        dotenv().unwrap_or_else(|error| panic!("Cannot load config buddy: {}", error));
+        dotenv().unwrap_or_else(|error| panic!("Cannot load config friend: {}", error));
     }
 }
