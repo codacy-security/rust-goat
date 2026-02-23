b82b264002874c78a0153418077e4493df3f119b
André Meira <andre.meira@codacy.com>
2026-02-23 10:19:19 +0000

    fix: Use better password

--- src/app/database/user.rs
+++ src/app/database/user.rs
@@ -46,7 +46,7 @@ mod tests {
 
     fn get_seed() -> NewUser {
         NewUser {
-            password: String::from("abcdef123"),
+            password: String::from("qwerty123"),
             username: String::from("test@example21.com"),
             name: None,
             surname: None,
